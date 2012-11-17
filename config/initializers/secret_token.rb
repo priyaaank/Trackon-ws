a2ab85f3d7e0659c2fcbc59ee2da21b77ec5786a
Priyank Gupta <priyaaank@gmail.com>
2012-11-17 21:14:53 +0530

    Adding initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackonWs::Application.config.secret_token = '6183ea156e7f5ae061c2c5c6dade5065a28af0c3e5d1faf754918296c3c74d7eeeca9287a692021051234a2f49f2b0e07fae930de0923d42801df8bf5ebe2e66'
