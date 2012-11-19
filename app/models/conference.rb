26ae0ca1dfa88628779fb9be7512be27c48bcd2d
Priyank Gupta <priyaaank@gmail.com>
2012-11-20 00:02:48 +0530

    Added a field to the conference model

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -3,6 +3,7 @@ class Conference
   include Mongoid::Document
 
   field :name, :type => String
+  field :description, :type => String
   
   embeds_one  :location, :as => :addressable
   embeds_many :ratings, :as => :rateable
